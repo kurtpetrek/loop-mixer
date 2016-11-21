40ba0bcad805b68dbf668af450fac081d0a6624e
kurtpetrek <kurtpetrek@Madelines-MacBook-Pro.local>
2016-11-21 16:39:55 -0500

    added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ var jsSources = [
   'components/scripts/comp2.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -44,6 +46,18 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
+});
+
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+  gulp.src(jsonSources)
+    .pipe(connect.reload())
 });
 
 gulp.task('connect', function() {
@@ -53,4 +67,4 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task ('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task ('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
